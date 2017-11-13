8a02e419e0b74a73ec911fd305a9c47d71585eff
gabriel <banded.gabriel@gmail.com>
2017-11-13 08:29:19 -0500

    regex updates. list differentiation

--- lists/tests.py
+++ lists/tests.py
@@ -2,17 +2,16 @@
 lists/tests.py
 """
 
-from django.urls import resolve
 from django.test import TestCase
-from lists.views import home_page
 from lists.models import Item
-
-from django.http import HttpRequest
-
-# Create your tests here.
 
 
 class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
 
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
@@ -23,9 +22,6 @@ class HomePageTest(TestCase):
         self.assertIn('itemey 1', response.content.decode())
         self.assertIn('itemey 2', response.content.decode())
 
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
@@ -34,14 +30,17 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
 
+
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+
 
 
 class ItemModelTest(TestCase):
