db785c6c25c25532e37413b631ef1962d4c21a95
gabriel <banded.gabriel@gmail.com>
2017-11-13 10:44:33 -0500

    some db changes

--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,7 @@ lists/views.py
 """
 
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 
@@ -18,5 +18,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
