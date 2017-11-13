2f3c3b3a01a342fd61379ccc6de39482ecd4db3e
gabriel <banded.gabriel@gmail.com>
2017-11-13 09:29:00 -0500

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -19,7 +19,6 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
